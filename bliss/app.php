3560b10ea6d0533d12a3d6647ff582e26f00d80c
Iain Edminster <iainedminster@gmail.com>
2014-12-03 19:43:08 -0700

    Adding documentation for magic methods used to get module instances

--- bliss/app.php
+++ bliss/app.php
@@ -1,14 +1,29 @@
 <?php
 require_once "core/Bliss/src/App/Container.php";
 
-class BlissApp
+/**
+ * # Core modules
+ * @method \Assets\Module assets() Assets module
+ * @method \Bliss\Module bliss() Bliss module
+ * @method \Config\Module config() Config module
+ * @method \Error\Module error() Error module
+ * @method \Request\Module request() Request module
+ * @method \Response\Module response() Response module
+ * @method \Router\Module router() Router module
+ * @method \View\Module view() View module
+ * 
+ * # Development Modules
+ * @method \Docs\Module docs() Docs module
+ * @method \Tests\Module tests() Tests module
+ */
+class BlissApp extends \Bliss\App\Container
 {
 	/**
-	 * @return \Bliss\App\Container
+	 * @return \BlissApp
 	 */
 	public static function create()
 	{
-		$instance = new Bliss\App\Container();
+		$instance = new self();
 		$instance->autoloader()->registerNamespace("Bliss", __DIR__ ."/core/Bliss/src");
 		$instance->modules()->registerModulesDirectory(__DIR__ ."/core");
 		

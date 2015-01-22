520d990700d9d2eb21ebe0da6826615231f7f665
Iain Edminster <iainedminster@gmail.com>
2015-01-21 18:56:51 -0700

    Simplifying application creation

--- bliss/app.php
+++ bliss/app.php
@@ -22,23 +22,82 @@ require_once "core/Bliss/src/App/Container.php";
  */
 class BlissApp extends \Bliss\App\Container
 {
+	private $startTime;
+	
+	public function __construct($name, $rootPath) 
+	{
+		parent::__construct($name, $rootPath);
+		
+		$this->startTime = microtime(true);
+	}
 	/**
 	 * @param string $name
 	 * @param string $rootPath
 	 * @return \BlissApp
 	 */
-	public static function create($name, $rootPath)
+	public static function create($name, $rootPath, $environment = self::ENV_PRODUCTION)
 	{
+		date_default_timezone_set("UTC");
+		
 		if (session_id() === "") {
 			session_start();
 		}
 		
+		// Create the application container
 		$instance = new self($name, $rootPath);
+		$instance->environment($environment);
 		$instance->autoloader()->registerNamespace("Bliss", __DIR__ ."/core/Bliss/src");
 		$instance->modules()->registerModulesDirectory(__DIR__ ."/core");
 		$instance->modules()->registerModulesDirectory(__DIR__ ."/web");
 		$instance->modules()->registerModulesDirectory(__DIR__ ."/security");
 		
+		if ($environment !== self::ENV_PRODUCTION) {
+			$instance->modules()->registerModulesDirectory(__DIR__ ."/development");
+		}
+		
+		// Setup error handling
+		error_reporting(-1);
+		ini_set("display_errors", true);
+		ini_set("display_startup_errors", true);
+		set_error_handler([$instance, "handleError"]);
+		set_exception_handler([$instance, "handleException"]);
+
 		return $instance;
+	}
+	
+	public function run()
+	{
+		// Setup the request
+		$baseUrl = preg_replace("/^(.*)\/.*\.php$/i", "\\1/", filter_input(INPUT_SERVER, "SCRIPT_NAME"));
+		$requestUri = substr(filter_input(INPUT_SERVER, "REQUEST_URI"), strlen($baseUrl));
+		$request = $this->request();
+		$request->setUri($requestUri);
+		$request->baseUrl($baseUrl);
+		
+		$router = $this->router();
+		$route = $router->find($requestUri);
+
+		$this->execute($route->params());
+	}
+	
+	public function __destruct() 
+	{
+		$format = $this->request()->getFormat();
+
+		if (in_array($format, [null, "html"])) {
+			echo "\n\n\n";
+			echo "<!-- Total Execution Time .............. ". number_format((microtime(true) - $this->startTime) * 1000, 2) ." ms -->\n";
+			echo "<!-- Total Memory Usage ................ ". number_format(memory_get_usage()/1024, 2) ." kb -->\n";
+
+			echo "\n\n";
+			echo "<!------------------------------>\n";
+			echo "<!--           Log            -->\n";
+			echo "<!------------------------------>\n";
+			echo "<!--\n\n";
+			foreach ($this->logs() as $log) {
+				echo "\t{$log["message"]}\n";
+			}
+			echo "-->";
+		}
 	}
 }
--- public/index.php
+++ public/index.php
@@ -1,52 +1,7 @@
 <?php
 require_once "../bliss/app.php";
 
-error_reporting(-1);
-ini_set("display_errors", true);
-ini_set("display_startup_errors", true);
-
-date_default_timezone_set("UTC");
-
-$startTime = microtime(true);
-$baseUrl = preg_replace("/^(.*)\/.*\.php$/i", "\\1/", filter_input(INPUT_SERVER, "SCRIPT_NAME"));
-$requestUri = substr(filter_input(INPUT_SERVER, "REQUEST_URI"), strlen($baseUrl));
-
-define("BASE_URL", $baseUrl);
-define("APP_ENV", getenv("APP_ENV") ? getenv("APP_ENV") : "development");
-
-$app = BlissApp::create("Bliss", dirname(__DIR__));
-$app->environment(APP_ENV);
+$env = getenv("APP_ENV") ? getenv("APP_ENV") : "development";
+$app = BlissApp::create("Bliss", dirname(__DIR__), $env);
 $app->modules()->registerModulesDirectory(dirname(__DIR__) ."/app");
-
-if (APP_ENV !== BlissApp::ENV_PRODUCTION) {
-	$app->modules()->registerModulesDirectory(dirname(__DIR__) ."/bliss/development");
-}
-
-set_error_handler([$app, "handleError"]);
-set_exception_handler([$app, "handleException"]);
-
-$request = $app->request();
-$request->setUri($requestUri);
-
-$router = $app->router();
-$route = $router->find($requestUri);
-
-$app->execute($route->params());
-
-$format = $request->getFormat();
-
-if (in_array($format, [null, "html"])) {
-	echo "\n\n\n";
-	echo "<!-- Total Execution Time .............. ". number_format((microtime(true) - $startTime) * 1000, 2) ." ms -->\n";
-	echo "<!-- Total Memory Usage ................ ". number_format(memory_get_usage()/1024, 2) ." kb -->\n";
-	
-	echo "\n\n";
-	echo "<!------------------------------>\n";
-	echo "<!--           Log            -->\n";
-	echo "<!------------------------------>\n";
-	echo "<!--\n\n";
-	foreach ($app->logs() as $log) {
-		echo "\t{$log["message"]}\n";
-	}
-	echo "-->";
-}
+$app->run();

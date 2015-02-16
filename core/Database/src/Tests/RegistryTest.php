e47aabfbf79f17cc886c7f97407392b910463487
Iain Edminster <iainedminster@gmail.com>
2015-02-16 14:43:34 -0800

    Typo fix

--- core/Database/src/Tests/RegistryTest.php
+++ core/Database/src/Tests/RegistryTest.php
@@ -8,7 +8,7 @@ class RegistryTest extends \PHPUnit_Framework_TestCase
 	public function testAddServer()
 	{
 		$registry = new Registry();
-		$registry->addServer("sqlite:memory");
+		$registry->addServer("sqlite::memory:");
 		
 		$this->assertEquals(1, $registry->totalServers());
 		$this->assertInstanceOf("\PDO", $registry->generateConnection());

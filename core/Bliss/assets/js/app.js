344c318782ddd246a652364abc55a1c474fe60c0
Iain Edminster <iainedminster@gmail.com>
2015-06-25 13:37:16 -0700

    Adding ability to specify addition modules

--- core/Bliss/assets/js/app.js
+++ core/Bliss/assets/js/app.js
@@ -1,7 +1,7 @@
-var bliss = angular.module("bliss", [
-	"ngRoute",
-	"ngResource"
-]);
+var blissModules = blissModules || [];
+blissModules.splice(0, 0, "ngRoute", "ngResource");
+
+var bliss = angular.module("bliss", blissModules);
 
 bliss.config(["$locationProvider", "$routeProvider", function($locationProvider, $routeProvider) {
 	$locationProvider.html5Mode(true);

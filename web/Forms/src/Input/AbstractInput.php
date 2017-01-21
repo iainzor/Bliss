3af3dedc7a18b89884775c0397b37e92f49deeac
Iain Edminster <iedminster@myiscorp.com>
2017-01-20 16:41:35 -0800

    Removing defaultValue property from inputs

--- web/Forms/src/Input/AbstractInput.php
+++ web/Forms/src/Input/AbstractInput.php
@@ -7,5 +7,16 @@ abstract class AbstractInput extends Component
 {
 	protected $type;
 	
+	/**
+	 * Get or set the default value of the input
+	 * 
+	 * @param mixed $defaultValue
+	 * @return mixed
+	 */
+	public function defaultValue($defaultValue = null)
+	{
+		return $this->getSet("defaultValue", $defaultValue);
+	}
+	
 	abstract public function type();
 }

cc375fbb65dfcd4524c10ad7593109f83c3bd6db
Iain Edminster <iainedminster@gmail.com>
2017-02-18 12:49:53 -0800

    Improvements to the test running Automatically checks for phpunit.phar and downloads the library if neccessary

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,6 +7,8 @@ require_once $root ."/modules/CmdLine/src/Application.php";
 date_default_timezone_set("America/Los_Angeles");
 
 $app = new CmdLine\Application();
+$app->moduleRegistry()->registerDirectory(__DIR__ . DIRECTORY_SEPARATOR . "PHPUnit");
+
 foreach (new DirectoryIterator($root ."/modules") as $item) {
 	if ($item->isDot() || $item->isFile()) {
 		continue;
--- tests/run.php
+++ tests/run.php
@@ -1,43 +1,60 @@
 <?php
-$bootstrap = __DIR__ . DIRECTORY_SEPARATOR ."bootstrap.php";
-$phpunitBootstrap = __DIR__ . DIRECTORY_SEPARATOR ."phpunit-bootstrap.php";
+define("ROOT_DIR", __DIR__ . DIRECTORY_SEPARATOR);
+define("PHPUNIT_DOWNLOAD_URL", "https://phar.phpunit.de/phpunit.phar");
 
 /* @var $app \CmdLine\Application */
-$app = include_once $bootstrap;
-$app->di()->call(function(Logs\Logger $logger) {
+$app = include_once ROOT_DIR . "bootstrap.php";
+$app->di()->call(function(Logs\Logger $logger) use ($app) {
 	$logger->registerOutput(new class() implements Logs\Output\OutputInterface {
 		public function next(Logs\Message\AbstractMessage $message) {
-			echo $message ."\n";
+			echo "> ". $message ."\n";
 		}
 	});
-});
-$app->moduleRegistry()->each(function(\Core\ModuleDefinition $module) use ($app, $phpunitBootstrap) {
-	$app->di()->register(Logs\Output\OutputInterface::class, new Logs\Output\PrintToBuffer());
-	$logger = $app->di()->get(Logs\Logger::class);
-	$testDir = $module->rootDir() . DIRECTORY_SEPARATOR ."tests";
 	
-	if (is_dir($testDir)) {
-		$logger->log("Running tests for module '". $module->getNamespace() ."' (". $module->rootDir() .")");
-		$startTime = microtime(true);
-		$command = implode(" ", [
-			"php",
-			"phpunit.phar",
-			'--bootstrap="'. $phpunitBootstrap .'"',
-			'"'. $module->rootDir() . DIRECTORY_SEPARATOR .'tests"'
-		]);
+	$pharPath = ROOT_DIR . "phpunit.phar";
+	$logger->log("Checking for phpunit.phar at '{$pharPath}'");
+	if (!is_file($pharPath)) {
+		$logger->log("PHPUnit not found, attempt to download...");
+		$from = fopen(PHPUNIT_DOWNLOAD_URL, "r");
+		$to = fopen($pharPath, "w");
 		
-		ob_start();
-		system($command);
-		$output = ob_get_clean();
-		$endTime = microtime(true);
-		$lines = explode("\n", $output);
-		$separator = "\n\t> ";
+		while ($line = fgets($from)) {
+			fputs($to, $line);
+		}
 		
-		echo $separator . implode($separator, $lines);
-		//echo "\n". str_repeat("=", 30) ."\n";
-		echo "\n\n";
-		
-		$seconds = number_format(($endTime - $startTime), 5);
-		$logger->log("Tests for module '". $module->getNamespace() ."' completed in {$seconds} seconds");
+		fclose($from);
+		fclose($to);
+		$logger->log("PHPUnit downloaded successfully!");
+	} else {
+		$logger->log("PHPUnit found, continuing with tests");
 	}
+	
+	$app->moduleRegistry()->each(function(\Core\ModuleDefinition $module) use ($app, $logger) {
+		$testDir = $module->rootDir() . DIRECTORY_SEPARATOR ."tests";
+
+		if (is_dir($testDir)) {
+			$logger->log("Running tests for module '". $module->getNamespace() ."' (". $module->rootDir() .")");
+			$startTime = microtime(true);
+			$command = implode(" ", [
+				"php",
+				"phpunit.phar",
+				'--bootstrap="'. ROOT_DIR .'phpunit-bootstrap.php"',
+				'"'. $module->rootDir() . DIRECTORY_SEPARATOR .'tests"'
+			]);
+
+			ob_start();
+			system($command);
+			$output = ob_get_clean();
+			$endTime = microtime(true);
+			$lines = explode("\n", $output);
+			$separator = "\n\t> ";
+
+			echo $separator . implode($separator, $lines);
+			//echo "\n". str_repeat("=", 30) ."\n";
+			echo "\n\n";
+
+			$seconds = number_format(($endTime - $startTime), 5);
+			$logger->log("Tests for module '". $module->getNamespace() ."' completed in {$seconds} seconds");
+		}
+	});
 });

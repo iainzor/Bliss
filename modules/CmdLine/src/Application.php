3f2f4a02d37bba7c7a2772948fac1ec995d7a035
Iain Edminster <iedminster@myiscorp.com>
2017-02-28 10:54:29 -0800

    Adding support for query parameters when calling a command line script

--- modules/CmdLine/src/Application.php
+++ modules/CmdLine/src/Application.php
@@ -12,24 +12,39 @@ class Application extends \Core\AbstractApplication
 	
 	public function run()
 	{
-		$options = getopt("m:c:a:");
+		$options = getopt("m:c:a:p::");
 		$moduleName = isset($options["m"]) ? $options["m"] : null;
 		$controllerName = isset($options["c"]) ? $options["c"] : null;
 		$actionName = isset($options["a"]) ? $options["a"] : null;
+		$queryString = isset($options["p"]) ? $options["p"] : "";
+		$params = $this->_parseQueryString($queryString);
 		
 		if ($moduleName === null || $controllerName === null || $actionName === null) {
 			throw new \Exception(
 				"No module, controller, or action provided.\n" .
-				"Usage: php exec.php -m [module] -c [controller] -a [action]\n"
+				"Usage: php exec.php -m [module] -c [controller] -a [action] [-p [params]]\n"
 			);
 		}
 		
 		try {
-			$result = $this->execute($moduleName, $controllerName, $actionName);
-			var_dump($result);
+			$result = $this->execute($moduleName, $controllerName, $actionName, $params);
+			echo $result;
 		} catch (\Exception $e) {
 			echo "Exception Encountered: {$e->getMessage()}\n\n";
 			echo $e->getTraceAsString();
 		}
+	}
+	
+	private function _parseQueryString(string $query) : array
+	{
+		$pairs = explode("&", $query);
+		$params = [];
+		foreach ($pairs as $pair) {
+			$parts = explode("=", $pair);
+			$name = $parts[0];
+			$value = isset($parts[1]) ? $parts[1] : null;
+			$params[$name] = $value;
+		}
+		return $params;
 	}
 }
